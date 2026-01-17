601e2b0068b7b68edf205d8f4354e75bb67dd092
Argenis_Developer <junarg21@gmail.com>
2026-01-17 16:44:07 -0300

    Deploy Constructora Navarro en Render

--- Backend/server.js
+++ Backend/server.js
@@ -3,20 +3,30 @@ import cors from "cors";
 import dotenv from "dotenv";
 import formRoutes from "./routes/formRoutes.js";
 import dataRoutes from "./routes/dataRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 
 
 dotenv.config();
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Middlewares
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // Rutas
 app.use("/api/form", formRoutes);
 app.use("/api/data", dataRoutes);
+app.get("*", (req, res) => {
+  if (!req.path.startsWith("/api")) {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  }
+});
 
 // Iniciar servidor
 const PORT = process.env.PORT || 4000;
